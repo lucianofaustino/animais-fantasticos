1357b14188436206caa679f6ece7ec0f4970fd36
Luciano Fastino <lucianofaustino10@gmail.com>
2022-05-15 15:48:57 -0300

    corrigido o caminho da api

--- js/script.js
+++ js/script.js
@@ -39,6 +39,6 @@ console.log(funcionamento);
 
 funcionamento.init();
 
-fetchAnimais('../../animaisapi.json', '.numeros-grid');
+fetchAnimais('./animaisapi.json', '.numeros-grid');
 
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
